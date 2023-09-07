a5ddbd73de87836aa16ed41cae0682636939e483
Blurry Face <kakashi.code.take@gmail.com>
2023-09-07 20:32:17 +0530

    Update:BrowserRouter basename

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename='/AnimeList'>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
